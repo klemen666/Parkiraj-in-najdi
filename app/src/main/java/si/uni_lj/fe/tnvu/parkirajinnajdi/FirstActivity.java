0846ef26129830417ee897b58d1f3865843363a5
asocialec <jernej.mihelj@gmail.com>
2015-06-29 16:31:38 +0200

    Dodal FirstActivity. Preveri, ce je prvi zagon in zastavico "locationFlag" (ce je lokacija shranjena). Ob prvem zagonu nastavi locationFlag na false in zazene MainActivity. Nato preveri locationFlag in zazene ali MainActivity ali GetWalkingDirection. // to ne deluje!!! povzroca sesuvanje aplikacije
    Se nekaj drugih sprememb.

---
+++ app/src/main/java/si/uni_lj/fe/tnvu/parkirajinnajdi/FirstActivity.java
@@ -0,0 +1,122 @@
+package si.uni_lj.fe.tnvu.parkirajinnajdi;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class FirstActivity extends ActionBarActivity {
+    String firstRunKey = "checkIfFirstRun";
+    String locationFlagKey = "LocationFlag";
+    String locationLatitudeKey = "locLatitude";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        // keys
+        firstRunKey = getResources().getString(R.string.firstRunKey);
+        locationFlagKey = getResources().getString(R.string.locationFlagKey);
+        locationLatitudeKey = getResources().getString(R.string.locationLatitudeKey);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_first, menu);
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // read from SharedPreferences
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(firstRunKey, 0);
+        int isFirstRun = sharedPref.getInt(firstRunKey, 1);
+
+        SharedPreferences locationFlagSharedPref = getApplicationContext().getSharedPreferences(locationFlagKey, 0);
+        // boolean locationFlag = locationFlagSharedPref.getBoolean(locationFlagKey, false);
+        boolean locationFlag = locationFlagSharedPref.getBoolean(locationFlagKey,false);
+
+        // check if first run
+        if (isFirstRun == 1){
+
+            Intent intent1 = new Intent(this, MainActivity.class);
+            startActivity(intent1);
+        }
+        else {
+            // check location flag
+
+            if (locationFlag == false) {
+                // start MainActivity
+                Intent intent1f = new Intent(this, MainActivity.class);
+                startActivity(intent1f);
+            }
+            if (locationFlag == true) {
+                // TO-DO: append location data (latitude and longitude)
+                Intent intent2 = new Intent(this, GetWalkingDirection.class);
+                startActivity(intent2);
+
+            }
+        }
+
+        // set isFirstRun to true
+        SharedPreferences.Editor editor = sharedPref.edit();
+        isFirstRun = 0;
+        editor.putInt(firstRunKey, isFirstRun);
+        editor.commit();
+    }
+/*
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // check location flag
+        SharedPreferences locationFlagSharedPref = getApplicationContext().getSharedPreferences(locationFlagKey, 0);
+        boolean locationFlag = locationFlagSharedPref.getBoolean(locationFlagKey, false);
+
+        if (locationFlag == false) {
+            // start MainActivity
+            Intent intent1f = new Intent(this, MainActivity.class);
+            startActivity(intent1f);
+        }
+        if (locationFlag == true) {
+            // TO-DO: append location data (latitude and longitude)
+            Intent intent2 = new Intent(this, GetWalkingDirection.class);
+            startActivity(intent2);
+
+        }
+    }
+*/
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/si/uni_lj/fe/tnvu/parkirajinnajdi/GetWalkingDirection.java
@@ -0,0 +1,104 @@
+package si.uni_lj.fe.tnvu.parkirajinnajdi;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class GetWalkingDirection extends ActionBarActivity {
+    String locationLatitudeKey = "locLatitude";
+    String locationLongitudeKey = "locLongitude";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_walking_direction);
+
+        locationLatitudeKey = getResources().getString(R.string.locationLatitudeKey);
+
+        /*
+        locationLatitudeKey = getResources().getString(R.string.locationLatitudeKey);
+        locationLongitudeKey = getResources().getString(R.string.locationLongitudeKey);
+
+        TextView latitudeTV = (TextView) findViewById(R.id.latitudeTV);
+        latitudeTV.setText("Latitude: ");
+
+        TextView longitudeTV = (TextView) findViewById(R.id.longitudeTV);
+        longitudeTV.setText("Longitude: ");
+        */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_get_walking_direction, menu);
+        return true;
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // read location and pass it to GetWalkingDirection
+
+        TextView izpis = (TextView) findViewById(R.id.izpisFlag);
+
+        SharedPreferences latitudeSharedPref = this.getSharedPreferences(locationLatitudeKey,0);
+        double latitude = Double.longBitsToDouble(latitudeSharedPref.getLong(locationLatitudeKey, 0));
+        izpis.setText("Latitude: " + latitude);
+
+        /*
+        if (locationFlag == true) {
+            izpis.setText("Flag = true");
+        }
+        else {
+            izpis.setText("Flag = false");
+        } */
+
+    /*
+        SharedPreferences latitudeSharedPrefKey = getApplicationContext().getSharedPreferences(locationLatitudeKey,0);
+        float latitude = latitudeSharedPrefKey.getFloat(locationLatitudeKey, 0);
+
+        SharedPreferences longitudeSharedPrefKey = getApplicationContext().getSharedPreferences(locationLongitudeKey,0);
+        float longitude = longitudeSharedPrefKey.getFloat(locationLongitudeKey, 0);
+
+
+        TextView latitudeTV = (TextView) findViewById(R.id.latitudeTV);
+        latitudeTV.setText("Latitude: " + latitude);
+
+        TextView longitudeTV = (TextView) findViewById(R.id.longitudeTV);
+        longitudeTV.setText("Longitude: " + longitude);
+    */
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
